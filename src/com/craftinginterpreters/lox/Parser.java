c601e28a7145fc6c2f04beadee7b6cfae304e26c
atdt <jasond1016@gmail.com>
2022-03-15 19:01:46 +0800

    10.6 Local Functions and Closures

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -55,6 +55,9 @@ class Parser {
         if (match(PRINT)) {
             return printStatement();
         }
+        if (match(RETURN)) {
+            return returnStatement();
+        }
         if (match(WHILE)) {
             return whileStatement();
         }
@@ -124,6 +127,18 @@ class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value");
         return new Stmt.Print(value);
+    }
+    
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
+
+        consume(SEMICOLON, "Expect ';' after return value.");
+        
+        return new Stmt.Return(keyword, value);
     }
 
     private Stmt whileStatement() {
